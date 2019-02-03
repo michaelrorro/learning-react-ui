8c81737f74d6921b8aa2d3e6387737cfe6bd6369
michael.rorro <michaelrorro@michaelrorros-MacBook-Pro.local>
2019-02-03 09:16:51 -0600

    udpated with jsx examples

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
-/* global React ReactDOM */
+import React from "react";
+import { render } from "react-dom";
+import { Pet } from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
     alert("You clicked this title...");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "?"
-      }),
-      React.createElement(Pet, {
-        name: "Fishy",
-        animal: "Fish",
-        breed: "Fish"
-      })
-    ]);
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+        <Pet name="Fishy" animal="Fish" breed="Angel Fish" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(
-  React.createElement(App, {}, null),
-  document.getElementById("root")
-);
+render(React.createElement(App, {}, null), document.getElementById("root"));
